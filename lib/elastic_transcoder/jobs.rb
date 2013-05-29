6bbf01e5a7865c4ab2dac24263f97041200c4b06
Chris Peters <chris@clearcrystalmedia.com>
2013-05-29 10:18:59 -0400

    Make Outputs:Rotate auto instead of 0

--- lib/elastic_transcoder/jobs.rb
+++ lib/elastic_transcoder/jobs.rb
@@ -31,7 +31,7 @@ module ElasticTranscoder
     #preset Generic 480p 16:9 => 1351620000000-000020
     def create_job input_key, output_key, pipeline_id, preset_id, thumbnail_pattern
       action = "jobs"
-      creation_params = '{"Input":{"Key":"'+input_key+'","FrameRate":"auto","Resolution":"auto","AspectRatio":"auto","Interlaced":"auto","Container":"auto"}, "Output":{"Key":"'+output_key+'","ThumbnailPattern":"'+thumbnail_pattern+'","Rotate":"0","PresetId":"'+preset_id+'"} ,"PipelineId":"'+pipeline_id+'"}'
+      creation_params = '{"Input":{"Key":"'+input_key+'","FrameRate":"auto","Resolution":"auto","AspectRatio":"auto","Interlaced":"auto","Container":"auto"}, "Output":{"Key":"'+output_key+'","ThumbnailPattern":"'+thumbnail_pattern+'","Rotate":"auto","PresetId":"'+preset_id+'"} ,"PipelineId":"'+pipeline_id+'"}'
       headers = @utilities.initialize_headers action, {:method=>"POST", :payload=>creation_params}
       url = @utilities.build_url action
       response = @utilities.execute_post url, headers, creation_params
